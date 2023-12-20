ef1a6448a47ab2df68e15bb011fde8535e3a5345
Ra <91133817+Dancingaroundthelies@users.noreply.github.com>
2023-12-20 23:24:55 +0700

    Update generate_key.php

--- generate_key.php
+++ generate_key.php
@@ -1,32 +1,20 @@
 <?php
-    $private_key = openssl_pkey_new();
-    $public_key_pem = openssl_pkey_get_details($private_key)['key'];
-    echo $public_key_pem;
-    $public_key = openssl_pkey_get_public($public_key_pem);
+    require_once('phpseclib/Crypt/RSA.php');
+    require_once('phpseclib/Math/BigInteger.php');
+    require_once('phpseclib/Crypt/Hash.php');
+    require_once('phpseclib/Crypt/Random.php');
+    use phpseclib\Crypt\RSA;
+    $rsa = new RSA();
+    $pair = $rsa->createKey();
+    $private_key = $pair['privatekey'];
+    $public_key = $pair['publickey'];
 
-    // ------------------------------------------------------------
-
-    // Generate key pair
-    $keyPair = openssl_pkey_new(array(
-        'private_key_bits' => 2048,
-        'private_key_type' => OPENSSL_KEYTYPE_RSA,
-    ));
-
-    // Get private key
-    openssl_pkey_export($keyPair, $privateKey);
-
-    // Get public key
-    $publicKeyDetails = openssl_pkey_get_details($keyPair);
-    $publicKey = $publicKeyDetails['key'];
-
-    // Encryption using public key
-    $plaintext = 'Hello, World!';
-    openssl_public_encrypt($plaintext, $encrypted, $publicKey);
-
-    // Decryption using private key
-    openssl_private_decrypt($encrypted, $decrypted, $privateKey);
-
-    echo "Original: $plaintext\n";
-    echo "Encrypted: " . base64_encode($encrypted) . "\n";
-    echo "Decrypted: $decrypted\n";
-?>
+    $plaintext = 'admin';
+    $rsa->loadKey($private_key);
+    $enc = base64_encode($rsa->encrypt($plaintext));
+    echo $enc;
+    echo "\n";
+    $rsa->loadKey($public_key);
+    $dec = $rsa->decrypt(base64_decode($enc));
+    echo $dec;
+    ?>
