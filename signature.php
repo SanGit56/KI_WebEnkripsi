fcde10caa0b9f4ce25fcfc9aefe6738226fb6599
Ra <91133817+Dancingaroundthelies@users.noreply.github.com>
2023-12-21 11:09:07 +0700

    Update signature.php

--- signature.php
+++ signature.php
@@ -8,11 +8,16 @@
     //import private key user
     $privsan = 'lyInbg69+4sGDiu9wDKKO7lrKkj58PrsN8VdxY+174PzuWSap+C/q4DPMdWP7JMmaXxQ88J29SAzBDic7geNgZhbERAODdkbMpc8yyXhIxA2j6EiQgxTE3aMShsEnfwPTCWXtlSZQBYFyKrJrR63bLPPMMqvKlor0XJVC/RDdV8=';
     $privkey = $privsan;
+    $todayDate = date('Y-m-d');
     $file = file_get_contents("Cheatsheet.pdf");
+    $lastm = filemtime($file);
     $hash = hash('sha256', $file);
     $rsa = new RSA();
     $rsa->loadKey($privkey);
     $header="====BEGIN_SIGNATURE====\n";
     $signature = base64_encode($rsa->encrypt($hash));
-    file_put_contents("Cheatsheett.pdf",$file."\n".$header.$signature);
+    $isi="Issuer        : San \n
+          Date          : $todayDate \n
+          Last Modified : $lastm \n";
+    file_put_contents("Cheatsheett.pdf",$file."\n".$header.$signature.$isi);
     
